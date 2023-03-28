5ad56a02cd5780e501b48019b8feee65eca5fd5d
gowtham-bonu-simform <gowtham@simformsolutions.com>
2023-03-28 17:44:33 +0530

    TE7-T120:ActiveRecordQueryInterface:Fix:made_changes

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -4,8 +4,8 @@ class EmployeesController < ApplicationController
   end
 
   def create
-    @employee = Employee.find_or_initialize_by(employee_params)
-    if @employee.save
+    @employee = Employee.find_or_create_by(employee_params)
+    if @employee
       redirect_to root_path
     else
       render :new, status: :unprocessable_entity
@@ -13,19 +13,10 @@ class EmployeesController < ApplicationController
   end
 
   def index
-    @employees = Employee.all.order(:id)
-    @email = params[:email_entered].strip if params[:email_entered]
-    @str = ""
-    @str2 = ""
-    if @email
-      @found_employee = Employee.find_by(email: @email)
-      if @found_employee
-        @str = @found_employee.first_name
-      else
-        @str = "no employee exists"
-      end
+    if params[:email_entered] and params[:commit] == "Search"
+      @employee = Employee.find_by(email: params[:email_entered].strip)
     else
-      @str = ""
+      @employees = Employee.all
     end
   end
 
@@ -44,7 +35,7 @@ class EmployeesController < ApplicationController
   end
 
   def increment
-    Employee.order(:id).limit(10).find_in_batches(batch_size: 1).each do |batch|
+    Employee.limit(10).find_in_batches(batch_size: 10).each do |batch|
       batch.each do |employee|
         employee.no_of_order += 1
         employee.save
@@ -54,7 +45,7 @@ class EmployeesController < ApplicationController
   end
 
   def decrement
-    Employee.order(:id).limit(10).find_in_batches(batch_size: 1).each do |batch|
+    Employee.limit(10).find_in_batches(batch_size: 10).each do |batch|
       batch.each do |employee|
         employee.no_of_order -= 1
         employee.save
@@ -71,17 +62,17 @@ class EmployeesController < ApplicationController
   end
 
   def results
-    @employees_age = Employee.order(:id).where(age: 20..40)
-    @employees_true = Employee.order(:id).where(full_time_available: true)
-    @employees_orders = Employee.order(:id).where(age: 26..).where(no_of_order: 5)
-    @employees_past = Employee.order(:id).where(created_at: ..Time.now.midnight-1)
-    @employees_less_orders = Employee.order(:id).where(age: ..24).or(Employee.order(:id).where(no_of_order: 5))
+    @employees_age = Employee.where(age: 20..40)
+    @employees_true = Employee.where(full_time_available: true)
+    @employees_orders = Employee.where("age > ? AND no_of_order = ?", 25, 5)
+    @employees_past = Employee.where(created_at: ..Time.now.midnight-1)
+    @employees_less_orders = Employee.where(age: ..24).or(Employee.order(:id).where(no_of_order: 5))
     @employees_desc = Employee.order(age: :desc)
     @employees_asc = Employee.order(:no_of_order)
     @employees_high = Employee.where(salary: 45001..)
-    @employees_group = Employee.select(:id, :first_name, :no_of_order).group(:id, :first_name, :no_of_order).where(no_of_order: 6..).order(:id)
-    @employees_unscope_false = Employee.order(:id).where(age: 50..)
-    @employees_unscope_true = Employee.order(:id).where(age: 50..).unscope(where: :age)
+    @employees_group = Employee.select(:id, :first_name, :no_of_order).group(:id, :first_name, :no_of_order).where(no_of_order: 6..)
+    @employees_unscope_false = Employee.where(age: 50..)
+    @employees_unscope_true = Employee.where(age: 50..).unscope(where: :age)
     @employees_only_false = Employee.order(id: :desc).limit(2)
     @employees_only_true = Employee.order(id: :desc).limit(2).only(:order)
     @employees_reselect_false = Employee.select(:first_name, :age, :salary)
